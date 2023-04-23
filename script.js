dd42cc442cad14a1939678eec298cc286fc98f2c
THANHTVU <111718979+THANHTVU@users.noreply.github.com>
2023-04-23 06:37:32 +0000

    HW3

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ let btn3 = document.getElementById("btn3");
 
 // Function to write the values of origArray into the 3 textboxes
 function writeValues() {
-  document.getElementById("txt1").value = origArray[0];
-  document.getElementById("txt2").value = origArray[1];
-  document.getElementById("txt3").value = origArray[2];
+  document.getElementById("numb1").value = origArray[0];
+  document.getElementById("numb2").value = origArray[1];
+  document.getElementById("numb3").value = origArray[2];
 }
 
 // Add event listener for DOMContentLoaded
@@ -19,9 +19,9 @@ document.addEventListener("DOMContentLoaded", function () {
   // Add click event listener to btn2 using anonymous function
   btn2.addEventListener("click", function () {
     let newArray = origArray.map((val) => val ** 3);
-    document.getElementById("txt1").value = newArray[0];
-    document.getElementById("txt2").value = newArray[1];
-    document.getElementById("txt3").value = newArray[2];
+    document.getElementById("numb1").value = newArray[0];
+    document.getElementById("numb2").value = newArray[1];
+    document.getElementById("numb3").value = newArray[2];
   });
 
   // Add click event listener to btn3 calling a named function
@@ -31,15 +31,15 @@ document.addEventListener("DOMContentLoaded", function () {
 // Function to calculate the square of values in origArray
 function square() {
   let newArray = origArray.map((val) => val ** 2);
-  document.getElementById("txt1").value = newArray[0];
-  document.getElementById("txt2").value = newArray[1];
-  document.getElementById("txt3").value = newArray[2];
+  document.getElementById("numb1").value = newArray[0];
+  document.getElementById("numb2").value = newArray[1];
+  document.getElementById("numb3").value = newArray[2];
 }
 
 // Function to calculate the 4th power of values in origArray
 function fourthPower() {
   let newArray = origArray.map((val) => val ** 4);
-  document.getElementById("txt1").value = newArray[0];
-  document.getElementById("txt2").value = newArray[1];
-  document.getElementById("txt3").value = newArray[2];
+  document.getElementById("numb1").value = newArray[0];
+  document.getElementById("numb2").value = newArray[1];
+  document.getElementById("numb3").value = newArray[2];
 }
